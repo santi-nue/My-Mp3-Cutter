251b3c2c382ea814a42cf2451ec446f5994fbd9b
Hamza Khalid <hamzachaudharyofficial@gmail.com>
2022-12-22 10:15:00 +0500

    Update gradle and add some utils code

--- app/src/main/java/com/example/mymp3cutter/PlayMusicActivity.java
+++ app/src/main/java/com/example/mymp3cutter/PlayMusicActivity.java
@@ -8,20 +8,12 @@ import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.provider.Settings;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.AlarmManagerCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
-
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +21,10 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -38,199 +34,207 @@ import java.io.IOException;
 
 public class PlayMusicActivity extends AppCompatActivity {
 
-	private Toolbar toolbar;
-	private AdView adView;
-	private TextView textViewFileName,textViewDuration;
-	private LinearLayout linearLayoutPlayPause;
-	private ImageView imageViewPlayPause;
-	private Button buttonRename,buttonDelete;
-	private MediaPlayer mediaPlayer;
-	private Uri uri;
-	private String filePath;
-	private File file;
-	private ContentValues contentValues;
-	private LinearLayout linearLayoutRingtone,linearLayoutContacts,linearLayoutNotifications,linearLayoutAlarm;
-	private static final int CODE_RINGTONE=101,CODE_NOTIFY=102,CODE_ALARM=103,CODE_CONTACT=104;
+    private Toolbar toolbar;
+    private AdView adView;
+    private TextView textViewFileName, textViewDuration;
+    private LinearLayout linearLayoutPlayPause;
+    private ImageView imageViewPlayPause;
+    private Button buttonRename, buttonDelete;
+    private MediaPlayer mediaPlayer;
+    private Uri uri;
+    private String filePath;
+    private File file;
+    private ContentValues contentValues;
+    private LinearLayout linearLayoutRingtone, linearLayoutContacts, linearLayoutNotifications, linearLayoutAlarm;
+    private static final int CODE_RINGTONE = 101, CODE_NOTIFY = 102, CODE_ALARM = 103, CODE_CONTACT = 104;
+     String fileName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_music);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_play_music);
+        textViewFileName = (TextView) findViewById(R.id.textViewFileName);
+        linearLayoutPlayPause = (LinearLayout) findViewById(R.id.linearLayoutPlayPause);
+        imageViewPlayPause = (ImageView) findViewById(R.id.iv_PlayPause);
+        textViewDuration = (TextView) findViewById(R.id.textViewDuration);
+        buttonRename = (Button) findViewById(R.id.button_rename);
+        buttonDelete = (Button) findViewById(R.id.button_delete);
+        linearLayoutRingtone = (LinearLayout) findViewById(R.id.ringtone);
+        linearLayoutAlarm = (LinearLayout) findViewById(R.id.alarm);
+        linearLayoutContacts = (LinearLayout) findViewById(R.id.contacts);
+        linearLayoutNotifications = (LinearLayout) findViewById(R.id.notification);
+        adView = (AdView) findViewById(R.id.adViewPlayMusic);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+          fileName = getIntent().getStringExtra(getResources().getString(R.string.music_file_name));
+        textViewFileName.setText(fileName);
+        uri = getIntent().getParcelableExtra(getResources().getString(R.string.selected_song_uri));
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_play_music);
-		toolbar=(Toolbar)findViewById(R.id.toolbar_play_music);
-		textViewFileName=(TextView)findViewById(R.id.textViewFileName);
-		linearLayoutPlayPause=(LinearLayout)findViewById(R.id.linearLayoutPlayPause);
-		imageViewPlayPause=(ImageView)findViewById(R.id.iv_PlayPause);
-		textViewDuration=(TextView)findViewById(R.id.textViewDuration);
-		buttonRename=(Button)findViewById(R.id.button_rename);
-		buttonDelete=(Button)findViewById(R.id.button_delete);
-		linearLayoutRingtone=(LinearLayout)findViewById(R.id.ringtone);
-		linearLayoutAlarm=(LinearLayout)findViewById(R.id.alarm);
-		linearLayoutContacts=(LinearLayout)findViewById(R.id.contacts);
-		linearLayoutNotifications=(LinearLayout)findViewById(R.id.notification);
-		adView=(AdView)findViewById(R.id.adViewPlayMusic);
-		setSupportActionBar(toolbar);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		final String fileName=getIntent().getStringExtra(getResources().getString(R.string.music_file_name));
-		textViewFileName.setText(fileName);
-		uri=getIntent().getParcelableExtra(getResources().getString(R.string.selected_song_uri));
+        filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Music/MyMp3Cutter/" + fileName;
+        file = new File(filePath);
 
-		filePath= Environment.getExternalStorageDirectory().getAbsolutePath()+"/Music/MyMp3Cutter/"+fileName;
-		file=new File(filePath);
-		contentValues=new ContentValues();
-		contentValues.put(MediaStore.MediaColumns.DATA,file.getAbsolutePath());
-		contentValues.put(MediaStore.MediaColumns.TITLE,fileName);
-		contentValues.put(MediaStore.MediaColumns.MIME_TYPE,"audio/ogg");
-		contentValues.put(MediaStore.Audio.Media.ARTIST,getResources().getString(R.string.app_name));
-		contentValues.put(MediaStore.Audio.Media.IS_RINGTONE,true);
-		contentValues.put(MediaStore.Audio.Media.IS_ALARM,true);
-		contentValues.put(MediaStore.Audio.Media.IS_MUSIC,true);
-		contentValues.put(MediaStore.Audio.Media.IS_RINGTONE,true);
 
-		linearLayoutPlayPause.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if(mediaPlayer.isPlaying()){
-					mediaPlayer.pause();
-					imageViewPlayPause.setImageResource(R.drawable.icon_play);
+        linearLayoutPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    imageViewPlayPause.setImageResource(R.drawable.icon_play);
 
-				}else{
-					mediaPlayer.start();
-					imageViewPlayPause.setImageResource(R.drawable.icon_pause);
-				}
-			}
-		});
-		buttonRename.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				final AlertDialog.Builder builder=new AlertDialog.Builder(PlayMusicActivity.this);
-				builder.setTitle(getResources().getString(R.string.rename_file));
-				final EditText editText=new EditText(PlayMusicActivity.this);
-				editText.setText(fileName);
-				builder.setView(editText);
-				builder.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
+                } else {
+                    mediaPlayer.start();
+                    imageViewPlayPause.setImageResource(R.drawable.icon_pause);
+                }
+            }
+        });
+        buttonRename.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(PlayMusicActivity.this);
+                builder.setTitle(getResources().getString(R.string.rename_file));
+                final EditText editText = new EditText(PlayMusicActivity.this);
+                editText.setText(fileName);
+                builder.setView(editText);
+                builder.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-						File newFile=new File(file.getParent()+"/"+editText.getText().toString());
-						file.renameTo(newFile);
-					}
-				});
-				builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-					}
-				});
-				builder.create().show();
-			}
+                        File newFile = new File(file.getParent() + "/" + editText.getText().toString());
+                        file.renameTo(newFile);
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
+            }
 
-		});
-		buttonDelete.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				AlertDialog.Builder builder=new AlertDialog.Builder(PlayMusicActivity.this);
-				builder.setMessage(R.string.are_you_sure);
-				builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						file.delete();
-						Intent intent=new Intent(PlayMusicActivity.this,MainActivity.class);
-						intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-						startActivity(intent);
-					}
-				});
-				builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-					}
-				});
-				builder.create().show();
-			}
-		});
-		linearLayoutContacts.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent =new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-				startActivityForResult(intent,CODE_CONTACT);
-			}
-		});
-		linearLayoutAlarm.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				setRingtone(RingtoneManager.TYPE_ALARM);
-			}
-		});
-		linearLayoutNotifications.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				setRingtone(RingtoneManager.TYPE_NOTIFICATION);
-			}
-		});
-		linearLayoutRingtone.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				setRingtone(RingtoneManager.TYPE_RINGTONE);
-			}
-		});
-	}
-	void setRingtone(int type){
-		Uri uri=MediaStore.Audio.Media.getContentUriForPath(file.getAbsolutePath());
-		getContentResolver().delete(uri, MediaStore.MediaColumns.DATA+"=\""+file.getAbsolutePath()+"\"",null);
-		Uri newUri1=getContentResolver().insert(uri,contentValues);
+        });
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(PlayMusicActivity.this);
+                builder.setMessage(R.string.are_you_sure);
+                builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        file.delete();
+                        Intent intent = new Intent(PlayMusicActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
+            }
+        });
+        linearLayoutContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, CODE_CONTACT);
+            }
+        });
+        linearLayoutAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRingtone(RingtoneManager.TYPE_ALARM);
+            }
+        });
+        linearLayoutNotifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRingtone(RingtoneManager.TYPE_NOTIFICATION);
+            }
+        });
+        linearLayoutRingtone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRingtone(RingtoneManager.TYPE_RINGTONE);
+            }
+        });
+    }
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-			if(Settings.System.canWrite(PlayMusicActivity.this)){
+    void setRingtone(int type) {
+        try {
+            Uri uri=MediaStore.Audio.Media.getContentUriForPath(file.getAbsolutePath());
+            Log.d("Utilsss", "setRingtone-> Uri: " + uri);
+            // getContentResolver().delete(uri, MediaStore.MediaColumns.DATA + "=\"" + file.getAbsolutePath() + "\"", null);
 
-				RingtoneManager.setActualDefaultRingtoneUri(PlayMusicActivity.this,type, newUri1);
-				Toast.makeText(this, getResources().getString(R.string.operation_success), Toast.LENGTH_SHORT).show();
-			}
-			else{
-				Intent intent=new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
-				startActivity(intent);
-			}
-		}
-	}
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		switch (requestCode){
-			case CODE_CONTACT:
-				if(resultCode==RESULT_OK){
-					Uri contactUri = data.getData();
-					ContentValues contentValues=new ContentValues();
-					contentValues.put(ContactsContract.Contacts.CUSTOM_RINGTONE,uri.toString());
-					int rowsUpdated=getContentResolver().update(contactUri,contentValues,null,null);
-					Toast.makeText(this, rowsUpdated+" contact updated", Toast.LENGTH_SHORT).show();
-				}
-		}
-	}
+            contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DATA, file.getAbsolutePath());
+            contentValues.put(MediaStore.MediaColumns.TITLE, fileName);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "audio/ogg");
+            contentValues.put(MediaStore.Audio.Media.ARTIST, getResources().getString(R.string.app_name));
+            contentValues.put(MediaStore.Audio.Media.IS_RINGTONE, true);
+            contentValues.put(MediaStore.Audio.Media.IS_ALARM, false);
+            contentValues.put(MediaStore.Audio.Media.IS_MUSIC, false);
+            contentValues.put(MediaStore.Audio.Media.IS_RINGTONE, false);
+            Uri newUri1 = getContentResolver().insert(uri, contentValues);
+            Log.d("Utilsss", "setRingtone-> newUri1: " + newUri1);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.System.canWrite(PlayMusicActivity.this)) {
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		AdRequest adRequest=new AdRequest.Builder().build();
-		adView.loadAd(adRequest);
-		mediaPlayer=new MediaPlayer();
-		mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-			@Override
-			public void onCompletion(MediaPlayer mp) {
-				imageViewPlayPause.setImageResource(R.drawable.icon_play);
-			}
-		});
-		try {
-			mediaPlayer.setDataSource(filePath);
-			mediaPlayer.prepare();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+                    RingtoneManager.setActualDefaultRingtoneUri(PlayMusicActivity.this, type, newUri1);
+                    Toast.makeText(this, getResources().getString(R.string.operation_success), Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                    startActivity(intent);
+                }
+            }
+        } catch (Exception e) {
+            Log.d("Utilsss", "setRingtone-> Exception: " + e);
+        }
+    }
 
-	@Override
-	protected void onStop() {
-		super.onStop();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case CODE_CONTACT:
+                if (resultCode == RESULT_OK) {
+                    Uri contactUri = data.getData();
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(ContactsContract.Contacts.CUSTOM_RINGTONE, uri.toString());
+                    int rowsUpdated = getContentResolver().update(contactUri, contentValues, null, null);
+                    Toast.makeText(this, rowsUpdated + " contact updated", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                imageViewPlayPause.setImageResource(R.drawable.icon_play);
+            }
+        });
+        try {
+            mediaPlayer.setDataSource(filePath);
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
 //		mediaPlayer.stop();
-		mediaPlayer.reset();
-		mediaPlayer.release();
-	}
+        mediaPlayer.reset();
+        mediaPlayer.release();
+    }
 }
---
+++ app/src/main/java/com/example/mymp3cutter/Utils.kt
@@ -0,0 +1,86 @@
+package com.example.mymp3cutter
+
+import android.content.ContentUris
+import android.content.Context
+import android.database.Cursor
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+
+/**
+ * Created by Hamza Chaudhary
+ * Sr. Software Engineer Android
+ * Created on 21 Dec,2022 20:05
+ * Copyright (c) All rights reserved.
+ */
+
+fun isMarshmallowPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+fun isNougatPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+fun isNougatMR1Plus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
+fun isOreoPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+fun isOreo() = Build.VERSION.SDK_INT == Build.VERSION_CODES.O
+fun isOreoMr1Plus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1
+fun isPiePlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+fun isQPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+fun isRPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
+
+
+fun Context.getMediaIDByPath(songPath: String): Long {
+    var id: Long = 0
+    val cr = contentResolver
+    val uri = if (isRPlus()) MediaStore.Audio.Media.getContentUri("external")
+    else MediaStore.Audio.Media.getContentUriForPath("external")
+
+    val selection = MediaStore.Audio.Media.DATA
+    val selectionArgs = arrayOf(songPath)
+    val projection = arrayOf(MediaStore.Audio.Media._ID)
+    val sortOrder = MediaStore.Audio.Media.TITLE + " ASC"
+    val cursor = uri?.let {
+        cr.query(
+            it, projection, "$selection=?", selectionArgs, null
+        )
+    }
+    if (cursor != null) {
+        while (cursor.moveToNext()) {
+            val idIndex = cursor.getColumnIndex(MediaStore.Audio.Media._ID)
+            id = cursor.getString(idIndex).toLong()
+        }
+    }
+    cursor?.close()
+    return id
+}
+
+fun Context.getURIFromPathString(path: String): Uri {
+    var id: Long = 0
+
+    val collection: Uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+    } else {
+        MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+    }
+    val projection = arrayOf(
+        MediaStore.Audio.Media._ID
+    )
+
+    val selection = MediaStore.Audio.Media.DATA + "=?"
+    val selectionArgs = arrayOf(
+        path
+    )
+
+    val cursor: Cursor? = contentResolver.query(
+        collection, projection, selection, selectionArgs, null
+    )
+    if (cursor != null) {
+        while (cursor.moveToNext()) {
+            val idIndex = cursor.getColumnIndex(MediaStore.Audio.Media._ID)
+            id = cursor.getString(idIndex).toLong()
+        }
+    }
+    cursor?.close()
+
+    return ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id)
+
+}
+
